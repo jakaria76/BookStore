5f097b5fb08825b7f17f5d8132ef0a6aeb798235
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2025-03-01 23:50:38 +0600

    Cart_component quantity_(+-)

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -9,11 +9,15 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 class CartComponent extends Component
 
 {
+
+    protected $listeners=['refreshComponent'=>'$refresh'];
+
     public function increaseQuantity($value)
     {
       $product=Cart::instance('cart')->get($value);
       $qty=$product->qty+1;
       Cart::instance('cart')->update($value,$qty);
+      $this->emitTo('carticon-component','refreshComponent');
     }
 
 
@@ -22,6 +26,7 @@ class CartComponent extends Component
       $product=Cart::instance('cart')->get($value);
       $qty=$product->qty-1;
       Cart::instance('cart')->update($value,$qty);
+      $this->emitTo('carticon-component','refreshComponent');
     }
 
 
@@ -29,6 +34,7 @@ class CartComponent extends Component
     {
       Cart::instance('cart')->remove($id);
       flash('Cart item has been remove');
+      $this->emitTo('carticon-component','refreshComponent');
     }
 
 
@@ -36,6 +42,7 @@ class CartComponent extends Component
     {
         Cart::instance('cart')->destroy();
         flash('All cart items has been remove');
+        $this->emitTo('carticon-component','refreshComponent');
 
     }
 
