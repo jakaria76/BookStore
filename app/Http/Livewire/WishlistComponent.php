6fa0651cb460537ffe0b6e43b5a015147c4e6573
MD.JAKARIA <jakariamahmud76@gmail.com>
2025-03-09 10:31:26 +0600

    WishlistComponentService add

--- app/Http/Livewire/WishlistComponent.php
+++ app/Http/Livewire/WishlistComponent.php
@@ -3,30 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use Gloudemans\Shoppingcart\Facades\Cart; // Make sure you're using the correct facade
+use App\Services\WishlistComponentService;
 
 class WishlistComponent extends Component
 {
+    protected $wishlistService;
+
+    public function __construct()
+    {
+        $this->wishlistService = app(WishlistComponentService::class);
+    }
+
     protected $listeners = ['refreshComponent' => '$refresh'];
+
     public function moveToCart($id)
     {
-        // Retrieve the item from the wishlist cart instance
-        $cart = Cart::instance('wishlist')->content()->get($id);
-        Cart::instance('wishlist')->remove($id);
-        Cart::instance('cart')->add($cart->id, $cart->name, 1, $cart->price)->associate('App\Models\Product');
-        $this->emitTo('wishlist-icon-component','refreshComponent');
-        $this->emitTo('carticon-component','refreshComponent');
+        $this->wishlistService->moveToCart($id);
+        $this->emitTo('wishlist-icon-component', 'refreshComponent');
+        $this->emitTo('carticon-component', 'refreshComponent');
         flash('Wishlist Item has been moved to cart.');
-
-
     }
 
     public function removewishlistProduct($id)
     {
-        Cart::instance('wishlist')->remove($id);
-        $this->emitTo('wishlist-icon-component','refreshComponent');
-        flash('Wishlist Item has been removed');
-
+        $this->wishlistService->removeWishlistProduct($id);
+        $this->emitTo('wishlist-icon-component', 'refreshComponent');
+        flash('Wishlist Item has been removed.');
     }
 
     public function render()
